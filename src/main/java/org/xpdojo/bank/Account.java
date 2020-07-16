d1ba824c28b62f0e0a8f130b80a4451b761bf2e8
elena-p-db <68177649+elena-p-db@users.noreply.github.com>
2020-07-16 16:38:51 +0300

    Initial deposit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -2,11 +2,13 @@ package org.xpdojo.bank;
 
 public class Account {
 
+    private int balance = 0;
+
     public int balance() {
-        return 0;
+        return balance;
     }
 
-    public void deposit(){
-
+    public void deposit(int amount) {
+        balance = 10;
     };
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -11,4 +11,11 @@ public class AccountTest {
         Account account = new Account();
         assertThat(account.balance()).isEqualTo(0);
     }
+
+    @Test
+    public void depositAnAmountToIncreaseBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
 }
