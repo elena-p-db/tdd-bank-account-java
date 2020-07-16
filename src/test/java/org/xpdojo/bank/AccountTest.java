d53b9586aa28b9439947329ef8c72e42f464e383
elena-p-db <68177649+elena-p-db@users.noreply.github.com>
2020-07-16 16:41:48 +0300

    Deposit multiple amount

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -18,4 +18,12 @@ public class AccountTest {
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(10);
     }
+
+    @Test
+    public void depositMultiAmountsToIncreaseBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
 }
